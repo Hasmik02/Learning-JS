66b3e9096216a3b92e7e21cbec775e859f36172f
Tigran <tigran.yengibaryan2004@gmail.com>
2021-03-14 21:45:18 +0400

    isStringStartsWithJava implementation

---
+++ fundamentals/isStringStartsWithJava.js
@@ -0,0 +1,9 @@
+function isStringStartsWithJava(str) {
+    return str.startsWith('Java');
+}
+
+// Tests
+ 
+// console.log(isStringStartsWithJava('Java'));
+// console.log(isStringStartsWithJava('JavaScript'));
+// console.log(isStringStartsWithJava('Jiva'));
